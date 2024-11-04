51c942f992671b5378680dfadad72e50adbc587c
lexaeliseev <lexaeliseev@gmail.com>
2024-11-04 17:59:41 +0300

    and attach and conftest

--- conftest.py
+++ conftest.py
@@ -1,38 +1,36 @@
 import pytest
-from selene import browser, Browser, Config
-from selenium import webdriver
+from selene import browser
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from utils import attach
 
 
-@pytest.fixture(autouse=True)
-def browser_settings():
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
-
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-
-
-@pytest.fixture(scope='function')
-def setup_browser(request):
+@pytest.fixture(scope="function")
+def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "124.0",
+        "browserVersion": "100.0",
         "selenoid:options": {
-            "enableVideo": True,
-            "enableVNC": True
+            "enableVNC": True,
+            "enableVideo": True
         }
     }
-
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
+    browser.config.driver = driver
 
-    browser = Browser(Config(driver))
+    browser.config.driver_name = "chrome"
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
 
-    yield browser
+    yield
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
 
     browser.quit()
