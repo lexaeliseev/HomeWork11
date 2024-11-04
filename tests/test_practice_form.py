475f1c4474427b332b88ffecd68e705ace01f6b2
lexaeliseev <lexaeliseev@gmail.com>
2024-11-04 18:50:06 +0300

    update

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -11,7 +11,7 @@ from pages.registration_page import RegistrationPage
 @allure.story("Заполнение формы регистрации")
 @allure.title("Отправка формы регистрации и проверка корректности отправленных значений")
 @allure.link("https://github.com", name="github")
-def test_practice_form():
+def test_practice_form(setup_browser):
     registration = RegistrationPage()
 
     with allure.step('Открыть браузер и перейти на сайт'):
